9e4081a484f7f76874ccaa29c796fb3ab42b93dc
John Ky <newhoggy@gmail.com>
2016-07-21 07:24:48 +1000

    Add ability to index json files

--- src/HaskellWorks/Data/LoadJson.hs
+++ src/HaskellWorks/Data/LoadJson.hs
@@ -8,6 +8,9 @@ import           Control.Monad
 import qualified Data.ByteString                                  as BS
 import qualified Data.Vector.Storable                             as DVS
 import           Data.Word
+import           Foreign.ForeignPtr
+import           Foreign.Ptr
+import           Foreign.Storable
 import           HaskellWorks.Data.Bits.BitShown
 import           HaskellWorks.Data.Decode
 import           HaskellWorks.Data.FromByteString
@@ -18,6 +21,19 @@ import           HaskellWorks.Data.Json.Succinct.PartialIndex
 import           HaskellWorks.Data.Json.Value
 import           HaskellWorks.Data.Succinct.BalancedParens.Simple
 import           HaskellWorks.Diagnostics.Time
+import           System.IO
+
+-- | Write out a vector verbatim into an open file handle.
+hPutVector :: forall a. Storable a => Handle -> DVS.Vector a -> IO ()
+hPutVector h v = withForeignPtr fp $ \p -> hPutBuf h (p `plusPtr` offset) sz
+      where
+        (fp, offset, n) = DVS.unsafeToForeignPtr v
+        eltsize = sizeOf (undefined :: a)
+        sz = n * eltsize
+
+-- | Write the vector verbatim to a file.
+writeVector :: forall a. Storable a => FilePath -> DVS.Vector a -> IO ()
+writeVector fp v = withFile fp WriteMode $ \h -> hPutVector h v
 
 readJson :: String -> IO (JsonCursor BS.ByteString (BitShown (DVS.Vector Word64)) (SimpleBalancedParens (DVS.Vector Word64)))
 readJson path = do
@@ -32,6 +48,14 @@ loadJson filename = do
   !cursor <- readJson filename
   let !jsonResult = (jsonIndexAt >=> jsonValueAt) cursor
   return $ (:[]) `fmap` jsonResult
+
+indexJson :: String -> IO ()
+indexJson filename = do
+  JsonCursor _ (BitShown ib) (SimpleBalancedParens bp) _ <- readJson filename
+  let wib = DVS.unsafeCast ib :: DVS.Vector Word8
+  let wbp = DVS.unsafeCast bp :: DVS.Vector Word8
+  writeVector (filename ++ ".ib") wib
+  writeVector (filename ++ ".bp") wbp
 
 loadJsonPartial :: String -> IO JsonPartialValue
 loadJsonPartial filename = do
