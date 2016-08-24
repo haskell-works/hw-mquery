99f2e9882e2b15d5a00864157d4014d65f65f64e
John Ky <newhoggy@gmail.com>
2016-08-24 23:27:39 +1000

    Remove JSON dependency

--- app/Main.hs
+++ app/Main.hs
@@ -3,27 +3,28 @@
 
 module Main where
 
-import           Control.Monad
-import qualified Data.DList as DL
-import           Data.Function
-import           HaskellWorks.Data.LoadJson
-import           HaskellWorks.Data.Micro
-import           HaskellWorks.Data.MQuery
+-- import           Control.Monad
+-- import qualified Data.DList as DL
+-- import           Data.Function
+-- import           HaskellWorks.Data.LoadJson
+-- import           HaskellWorks.Data.Micro
+-- import           HaskellWorks.Data.MQuery
 
 main :: IO ()
 main = do
-  !json <- loadJsonWithPoppy512SMinMaxIndex "firehose.json"
-  let q = MQuery (DL.singleton json)
-  putPretty $ q >>= (
-    item  >=> entry >=> named "attack"
-          >=> entry >=> named "instances"
-          >=> entry >=> named "instance"
-          >=> item
-          >=> entry >=> named "dataset"
-          >=> item >=> having (entry >=> named "name") >=> having (entry >=> named "name" >=> asString >=> valueOf "protos_bytes")
-          >=> entry >=> named "item"
-          >=> item >=> having (entry >=> named "name" >=> asString >=> valueOf "tcp")
-          >=> entry >=> named "class"
-          >=> entry >=> named "current"
-          >=> entry >=> named "value"
-          >=> castAsInteger) & aggregate sum
+  putStrLn "Hello world"
+  -- !json <- loadJsonWithPoppy512SMinMaxIndex "firehose.json"
+  -- let q = MQuery (DL.singleton json)
+  -- putPretty $ q >>= (
+  --   item  >=> entry >=> named "attack"
+  --         >=> entry >=> named "instances"
+  --         >=> entry >=> named "instance"
+  --         >=> item
+  --         >=> entry >=> named "dataset"
+  --         >=> item >=> having (entry >=> named "name") >=> having (entry >=> named "name" >=> asString >=> valueOf "protos_bytes")
+  --         >=> entry >=> named "item"
+  --         >=> item >=> having (entry >=> named "name" >=> asString >=> valueOf "tcp")
+  --         >=> entry >=> named "class"
+  --         >=> entry >=> named "current"
+  --         >=> entry >=> named "value"
+  --         >=> castAsInteger) & aggregate sum
